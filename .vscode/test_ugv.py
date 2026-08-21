db82539cf6252f4456d96d57ea389cc142bf1e56
Young Moon <noah4679@naver.com>
2026-08-21 21:29:17 +0900

    테스트

---
+++ .vscode/test_ugv.py
@@ -0,0 +1,18 @@
+import serial
+import threading
+
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1, dsrdtr=None)
+ser.setRTS(False)
+ser.setDTR(False)
+
+def read_serial():
+    while True:
+        data = ser.readline()
+        if data:
+            print("RX:", data.decode(errors="replace").strip())
+
+threading.Thread(target=read_serial, daemon=True).start()
+
+while True:
+    cmd = input("TX> ")
+    ser.write((cmd + "\n").encode())
