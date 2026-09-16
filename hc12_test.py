2e2b12383abd994ea03ac2389d7c1228162b91d6
SH <tjgus3213@gmail.com>
2026-09-16 13:57:47 +0900

    a

--- hc12_test.py
+++ hc12_test.py
@@ -1,10 +1,27 @@
 import serial
+import threading
 
-hc12 = serial.Serial('/dev/ttyTHS1', 115200, timeout=2)
+hc12 = serial.Serial(
+    '/dev/ttyTHS1',
+    115200,
+    timeout=0.1
+)
 
-print("HC12 TEST")
+print("JETSON HC12 READY")
+
+def receive():
+    while True:
+        if hc12.in_waiting:
+            data = hc12.read(hc12.in_waiting)
+
+            if data:
+                text = data.decode('utf-8', errors='ignore')
+                print("\nRX:", text, end="")
+                print("SEND >", end=" ", flush=True)
+
+threading.Thread(target=receive, daemon=True).start()
 
 while True:
-    data = hc12.readline()
-    if data:
-        print("RX:", repr(data))
+    text = input("SEND > ")
+    hc12.write((text + '\n').encode('utf-8'))
+    hc12.flush()
