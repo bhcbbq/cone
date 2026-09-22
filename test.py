6d5c1ed6b06ae164f6b446419a6b2edbe3997e2c
김주만 <juman6161@naver.com>
2026-09-22 11:23:33 +0900

    테스트

--- test.py
+++ test.py
@@ -1,65 +1,28 @@
 import Jetson.GPIO as GPIO
 import time
 
-TRIG = 12
 ECHO = 16
 
 GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(TRIG, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(ECHO, GPIO.IN)
 
-print("ECHO 신호 테스트")
-print("Ctrl+C 로 종료")
+print("ECHO 평상시 상태 테스트")
+print("10초 동안 ECHO 상태를 확인합니다.")
 print()
 
 try:
-    while True:
+    for i in range(100):
+        state = GPIO.input(ECHO)
 
-        # TRIG 신호 발생
-        GPIO.output(TRIG, GPIO.LOW)
-        time.sleep(0.000002)
-
-        GPIO.output(TRIG, GPIO.HIGH)
-        time.sleep(0.000010)
-
-        GPIO.output(TRIG, GPIO.LOW)
-
-        # ECHO가 HIGH가 되는지 확인
-        start = time.perf_counter()
-
-        echo_detected = False
-
-        while time.perf_counter() - start < 0.05:
-
-            if GPIO.input(ECHO) == GPIO.HIGH:
-                echo_detected = True
-                break
-
-        if echo_detected:
-            print("★ ECHO HIGH 감지!")
-
-            # HIGH가 유지되는 시간 측정
-            pulse_start = time.perf_counter()
-
-            while GPIO.input(ECHO) == GPIO.HIGH:
-                if time.perf_counter() - pulse_start > 0.05:
-                    break
-
-            pulse_end = time.perf_counter()
-
-            pulse_width = pulse_end - pulse_start
-
-            print("ECHO 시간: {:.6f} sec".format(pulse_width))
-            print()
-
+        if state == GPIO.HIGH:
+            print("ECHO = HIGH")
         else:
-            print("ECHO 없음")
+            print("ECHO = LOW")
 
         time.sleep(0.1)
 
 except KeyboardInterrupt:
-    print("\n종료")
+    pass
 
 finally:
     GPIO.cleanup()
