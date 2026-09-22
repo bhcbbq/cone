79934ef6bdea0132eab140b2e8e6e73a67098a40
김주만 <juman6161@naver.com>
2026-09-22 11:30:35 +0900

    테스트 2

--- test.py
+++ test.py
@@ -6,25 +6,16 @@ TRIG = 12
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(TRIG, GPIO.OUT, initial=GPIO.LOW)
 
-print("TRIG 테스트 시작")
-print("10번의 트리거 신호를 보냅니다.")
+print("TRIG LOW 상태")
+time.sleep(2)
 
-try:
-    for i in range(10):
-        print("TRIG 신호 {} / 10".format(i + 1))
+print("TRIG HIGH 상태 - 5초 유지")
+GPIO.output(TRIG, GPIO.HIGH)
+time.sleep(5)
 
-        GPIO.output(TRIG, GPIO.LOW)
-        time.sleep(0.000002)
+print("TRIG LOW 상태")
+GPIO.output(TRIG, GPIO.LOW)
+time.sleep(2)
 
-        GPIO.output(TRIG, GPIO.HIGH)
-        time.sleep(0.000020)
-
-        GPIO.output(TRIG, GPIO.LOW)
-
-        time.sleep(0.1)
-
-finally:
-    GPIO.output(TRIG, GPIO.LOW)
-    GPIO.cleanup()
-
+GPIO.cleanup()
 print("테스트 종료")
