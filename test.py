6444389fa902b1637296d4b4088687382d00539f
김주만 <juman6161@naver.com>
2026-09-22 11:20:41 +0900

    테스트4

--- test.py
+++ test.py
@@ -9,19 +9,14 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(TRIG, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(ECHO, GPIO.IN)
 
-print("JSN-SR04T 테스트")
-print("TRIG = Pin 12")
-print("ECHO = Pin 16")
-print("종료: Ctrl+C")
+print("ECHO 신호 테스트")
+print("Ctrl+C 로 종료")
 print()
 
 try:
     while True:
 
-        # 센서 안정화
-        time.sleep(0.06)
-
-        # TRIG 10us
+        # TRIG 신호 발생
         GPIO.output(TRIG, GPIO.LOW)
         time.sleep(0.000002)
 
@@ -30,40 +25,41 @@ try:
 
         GPIO.output(TRIG, GPIO.LOW)
 
-        # ECHO가 HIGH가 되기를 기다림
-        start_wait = time.perf_counter()
+        # ECHO가 HIGH가 되는지 확인
+        start = time.perf_counter()
 
-        while GPIO.input(ECHO) == GPIO.LOW:
-            if time.perf_counter() - start_wait > 0.07:
+        echo_detected = False
+
+        while time.perf_counter() - start < 0.05:
+
+            if GPIO.input(ECHO) == GPIO.HIGH:
+                echo_detected = True
                 break
 
-        # 정말 HIGH가 되었는지 확인
-        if GPIO.input(ECHO) == GPIO.LOW:
-            print("ECHO 없음")
-            continue
+        if echo_detected:
+            print("★ ECHO HIGH 감지!")
 
-        # HIGH 시작
-        echo_start = time.perf_counter()
+            # HIGH가 유지되는 시간 측정
+            pulse_start = time.perf_counter()
 
-        # ECHO가 LOW가 되기를 기다림
-        while GPIO.input(ECHO) == GPIO.HIGH:
-            if time.perf_counter() - echo_start > 0.07:
-                break
+            while GPIO.input(ECHO) == GPIO.HIGH:
+                if time.perf_counter() - pulse_start > 0.05:
+                    break
 
-        echo_end = time.perf_counter()
+            pulse_end = time.perf_counter()
 
-        # 실제 ECHO HIGH 시간
-        pulse_time = echo_end - echo_start
+            pulse_width = pulse_end - pulse_start
 
-        distance = pulse_time * 34300 / 2
+            print("ECHO 시간: {:.6f} sec".format(pulse_width))
+            print()
 
-        if distance < 20 or distance > 600:
-            print("비정상 측정: {:.1f} cm".format(distance))
         else:
-            print("거리: {:.1f} cm".format(distance))
+            print("ECHO 없음")
+
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
-    print("\n프로그램 종료")
+    print("\n종료")
 
 finally:
     GPIO.cleanup()
