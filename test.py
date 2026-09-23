1aa25d660a837d6ccea545ed15fa441056a1caec
김주만 <juman6161@naver.com>
2026-09-23 09:22:26 +0900

    테스트

--- test.py
+++ test.py
@@ -1,42 +1,30 @@
 import Jetson.GPIO as GPIO
 import time
 
-TRIG = 12
 ECHO = 16
 
 GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(TRIG, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(ECHO, GPIO.IN)
 
-print("ECHO 테스트 시작")
-print("센서 앞에 약 40~50cm 거리의 물체를 놓으세요.")
+print("Jetson ECHO 수신 테스트")
+print("Arduino가 TRIG를 보내고 있습니다.")
 
 try:
     while True:
 
-        # TRIG 신호
-        GPIO.output(TRIG, GPIO.LOW)
-        time.sleep(0.000002)
-
-        GPIO.output(TRIG, GPIO.HIGH)
-        time.sleep(0.000010)
-
-        GPIO.output(TRIG, GPIO.LOW)
-
-        # ECHO가 HIGH가 되기를 기다림
+        # ECHO HIGH 대기
         start_wait = time.time()
 
         while GPIO.input(ECHO) == GPIO.LOW:
             if time.time() - start_wait > 0.1:
-                print("ECHO HIGH 없음")
+                print("ECHO 없음")
                 break
 
         else:
             # ECHO HIGH 시작
             start = time.time()
 
-            # ECHO가 LOW가 되기를 기다림
+            # ECHO LOW 대기
             while GPIO.input(ECHO) == GPIO.HIGH:
                 if time.time() - start > 0.1:
                     print("ECHO LOW 없음")
@@ -53,11 +41,10 @@ try:
                     .format(echo_time, distance)
                 )
 
-        time.sleep(0.2)
+        time.sleep(0.05)
 
 except KeyboardInterrupt:
     print("\n테스트 종료")
 
 finally:
-    GPIO.output(TRIG, GPIO.LOW)
     GPIO.cleanup()
