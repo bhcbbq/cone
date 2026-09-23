5365ad92ba86ebc7201983ac8a80a8c2b446fc69
김주만 <juman6161@naver.com>
2026-09-23 09:39:29 +0900

    테스트2

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 import Jetson.GPIO as GPIO
 import time
 
-# =========================
-# GPIO 설정
-# =========================
 TRIG = 12
 ECHO = 16
 
@@ -12,102 +9,41 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(TRIG, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(ECHO, GPIO.IN)
 
-# 실제 테스트에서 20cm 이하에서 불안정했으므로
-# 20cm를 실질적인 최소 유효 거리로 설정
-MIN_DISTANCE = 20.0
-
-# JSN-SR04T 계열의 일반적인 최대 범위를 고려
-MAX_DISTANCE = 600.0
-
-print("================================")
-print(" JSN-SR04T 거리 측정 시작")
-print(" TRIG : Jetson Pin 12")
-print(" ECHO : Jetson Pin 16")
-print("================================")
+print("ECHO 상태 확인 시작")
+print("TRIG = Pin 12")
+print("ECHO = Pin 16")
+print()
 
 try:
-
     while True:
-
-        # ---------------------------------
-        # 이전 ECHO 신호가 남아있다면 기다림
-        # ---------------------------------
-        wait_start = time.monotonic()
-
-        while GPIO.input(ECHO) == GPIO.HIGH:
-
-            if time.monotonic() - wait_start > 0.1:
-                break
-
-        # ---------------------------------
-        # TRIG 신호
-        # ---------------------------------
+        # TRIG 신호 발생
         GPIO.output(TRIG, GPIO.LOW)
         time.sleep(0.000002)
 
         GPIO.output(TRIG, GPIO.HIGH)
-        time.sleep(0.000012)   # 12us
+        time.sleep(0.000012)
+
         GPIO.output(TRIG, GPIO.LOW)
 
-        # ---------------------------------
-        # ECHO HIGH 시작 대기
-        # ---------------------------------
-        start_wait = time.monotonic()
+        # ECHO 상태를 일정 시간 확인
+        start = time.monotonic()
 
-        while GPIO.input(ECHO) == GPIO.LOW:
+        echo_detected = False
 
-            if time.monotonic() - start_wait > 0.06:
+        while time.monotonic() - start < 0.1:
+            if GPIO.input(ECHO) == GPIO.HIGH:
+                echo_detected = True
+                print(">>> ECHO HIGH 감지!")
                 break
 
-        else:
+        if not echo_detected:
+            print("ECHO 없음")
 
-            # ---------------------------------
-            # ECHO HIGH 시작
-            # ---------------------------------
-            start = time.monotonic()
-
-            # ---------------------------------
-            # ECHO LOW 대기
-            # ---------------------------------
-            while GPIO.input(ECHO) == GPIO.HIGH:
-
-                if time.monotonic() - start > 0.06:
-                    break
-
-            else:
-
-                end = time.monotonic()
-
-                # ---------------------------------
-                # 거리 계산
-                # ---------------------------------
-                echo_time = end - start
-
-                distance = echo_time * 34300.0 / 2.0
-
-                # ---------------------------------
-                # 비정상 값 제거
-                # ---------------------------------
-                if MIN_DISTANCE <= distance <= MAX_DISTANCE:
-
-                    print(
-                        "거리: {:.2f} cm".format(distance)
-                    )
-
-                else:
-
-                    print(
-                        "측정 범위 밖: {:.2f} cm".format(distance)
-                    )
-
-        # JSN-SR04T 권장 측정 간격 확보
-        time.sleep(0.06)
+        time.sleep(0.2)
 
 except KeyboardInterrupt:
-
-    print("\n측정 종료")
+    print("\n종료")
 
 finally:
-
     GPIO.output(TRIG, GPIO.LOW)
     GPIO.cleanup()
